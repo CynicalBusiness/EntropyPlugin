88e487157bda91b2be25103cc7146eb2cef0426b
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-14 13:10:21 -0700

    Added CoreUtil utility class.

---
+++ src/me/capit/entropy/util/CoreUtil.java
@@ -0,0 +1,15 @@
+package me.capit.entropy.util;
+
+import me.capit.entropy.EntropyMain;
+
+public class CoreUtil {
+	final EntropyMain plugin;
+	public CoreUtil(EntropyMain plugin){
+		this.plugin = plugin;
+	}
+	
+	public boolean areaEmpty(){
+		return false;
+	}
+	
+}
