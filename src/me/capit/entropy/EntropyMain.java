cac4d78eb36c8d67a088ced3b86c98ee520873e9
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-14 12:51:46 -0700

    Added main class.

--- src/me/capit/entropy/EntropyMain.java
+++ src/me/capit/entropy/EntropyMain.java
@@ -1,12 +1,20 @@
 package me.capit.entropy;
 
+import java.util.logging.Logger;
+
+import me.capit.ds_mc.DSMCMain;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EntropyMain extends JavaPlugin {
+	public static Logger logger;
 	
 	@Override
 	public void onEnable(){
+		logger = this.getLogger();
+		logger.info(DSMCMain.formHeader(EntropyMain.class));
 		
+		// TODO
 	}
 	
 	@Override
