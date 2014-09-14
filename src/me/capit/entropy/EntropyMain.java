b11e1ab47c02b5a8022c523af590c4bed0d31eb0
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-14 13:10:01 -0700

    Added CoreUtil utility class.

--- src/me/capit/entropy/EntropyMain.java
+++ src/me/capit/entropy/EntropyMain.java
@@ -3,16 +3,21 @@ package me.capit.entropy;
 import java.util.logging.Logger;
 
 import me.capit.ds_mc.DSMCMain;
+import me.capit.entropy.util.CoreUtil;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EntropyMain extends JavaPlugin {
 	public static Logger logger;
+	public static CoreUtil util;
 	
 	@Override
 	public void onEnable(){
 		logger = this.getLogger();
 		logger.info(DSMCMain.formHeader(EntropyMain.class));
+		
+		logger.info("Loading utilities...");
+		util = new CoreUtil(this);
 		
 		// TODO
 	}
