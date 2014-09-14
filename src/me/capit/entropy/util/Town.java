fe99a2eb1aef9cc90ebddeecf62bed09e2ebc603
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-14 13:30:10 -0700

    Added some data to the Town class.

--- src/me/capit/entropy/util/Town.java
+++ src/me/capit/entropy/util/Town.java
@@ -1,6 +1,7 @@
 package me.capit.entropy.util;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 import me.capit.entropy.EntropyMain;
 
@@ -8,8 +9,35 @@ public class Town implements Serializable {
 	private static final long serialVersionUID = -8979383970183615150L;
 	final EntropyMain plugin;
 	
-	public Town(EntropyMain plugin){
+	private final UUID owner;
+	
+	private int daysOfInactivityLeft = 7;
+	private String name;
+	
+	public Town(EntropyMain plugin, UUID owner, String name){
 		this.plugin = plugin;
+		this.owner = owner;
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public UUID getOwner(){
+		return owner;
+	}
+	
+	public void inactiveDay(){
+		daysOfInactivityLeft--;
+	}
+	
+	public int getInactiveDays(){
+		return daysOfInactivityLeft;
+	}
+	
+	public void setInactiveDays(int days){
+		daysOfInactivityLeft = days;
 	}
 	
 }
