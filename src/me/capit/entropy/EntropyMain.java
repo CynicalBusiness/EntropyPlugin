5d3bf55cd221f206fc3bbaf40ab05e7f67fa88ea
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-14 12:38:56 -0700

    First plugin commit.

---
+++ src/me/capit/entropy/EntropyMain.java
@@ -0,0 +1,17 @@
+package me.capit.entropy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EntropyMain extends JavaPlugin {
+	
+	@Override
+	public void onEnable(){
+		
+	}
+	
+	@Override
+	public void onDisable(){
+		
+	}
+	
+}
