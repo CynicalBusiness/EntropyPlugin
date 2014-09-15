5138189f7e2ee19ef7345a87baa0b7c9c75d8c6d
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-15 16:26:51 -0700

    Templace Schematic class.

---
+++ src/me/capit/entropy/util/Schematic.java
@@ -0,0 +1,10 @@
+package me.capit.entropy.util;
+
+import java.io.Serializable;
+
+public class Schematic implements Serializable {
+	private static final long serialVersionUID = 5001691722785174168L;
+	
+	// TODO!
+
+}
