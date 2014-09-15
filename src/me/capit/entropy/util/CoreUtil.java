c1b1e4684ac8ed0b5131bc995bc70598a3d2a992
Matthew Struble <Matt@MayateckInnovations.net>
2014-09-15 16:27:12 -0700

    Added areaEmpty to utility class.

--- src/me/capit/entropy/util/CoreUtil.java
+++ src/me/capit/entropy/util/CoreUtil.java
@@ -1,5 +1,9 @@
 package me.capit.entropy.util;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+
 import me.capit.entropy.EntropyMain;
 
 public class CoreUtil {
@@ -8,7 +12,20 @@ public class CoreUtil {
 		this.plugin = plugin;
 	}
 	
-	public boolean areaEmpty(){
+	public boolean areaEmpty(World world, Location corner1, Location corner2){
+		for (int y=corner1.getBlockY();y<corner2.getBlockY();y++){
+			for (int x=corner1.getBlockX();x<corner2.getBlockX();x++){
+				for (int z=corner1.getBlockZ();z<corner2.getBlockZ();z++){
+					if (world.getBlockAt(x, y, z).getType()!=Material.AIR) return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+
+	public boolean structureFits(World world, Structure structure, Location loc){
+		
 		return false;
 	}
 	
