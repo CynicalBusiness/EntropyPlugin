24c22b50c83d5027c09b2f7ee19e97db62593a3f
NukesForKids <matt@capit.me>
2014-09-17 19:39:31 -0700

    Tweaked CoreUtil slightly.

--- src/me/capit/entropy/util/CoreUtil.java
+++ src/me/capit/entropy/util/CoreUtil.java
@@ -13,19 +13,23 @@ public class CoreUtil {
 		this.plugin = plugin;
 	}
 
-	public boolean areaEmpty(World world, Location corner1, Location corner2) {
-		for (int y = corner1.getBlockY(); y < corner2.getBlockY(); y++) {
-			
-			for (int x = corner1.getBlockX(); x < corner2.getBlockX(); x++) {
-				
-				for (int z = corner1.getBlockZ(); z < corner2.getBlockZ(); z++) {
-					
-					if (world.getBlockAt(x, y, z).getType() != Material.AIR)
-						return false;
+	public boolean areaOnlyContains(Location corner1, Location corner2, Material mat){
+		if (corner1.getWorld().getName().equalsIgnoreCase(corner2.getWorld().getName())){
+			for (int y = corner1.getBlockY(); y < corner2.getBlockY(); y++) {
+				for (int x = corner1.getBlockX(); x < corner2.getBlockX(); x++) {
+					for (int z = corner1.getBlockZ(); z < corner2.getBlockZ(); z++) {
+						if (corner1.getWorld().getBlockAt(x, y, z).getType() != mat) return false;
+					}
 				}
 			}
+			return true;
+		} else {
+			throw new IllegalArgumentException("Location's worlds don't match.");
 		}
-		return true;
+	}
+	
+	public boolean areaEmpty(Location corner1, Location corner2){
+		return areaOnlyContains(corner1,corner2,Material.AIR);
 	}
 
 	public boolean structureFits(Structure structure, Location loc) {
