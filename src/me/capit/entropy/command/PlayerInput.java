d9a7da250e43f720ff7b3141320626ded038bf8e
NukesForKids <matt@capit.me>
2014-09-20 07:53:21 -0700

    Improved upon the PlayerInput class.

--- src/me/capit/entropy/command/PlayerInput.java
+++ src/me/capit/entropy/command/PlayerInput.java
@@ -1,10 +1,15 @@
 package me.capit.entropy.command;
 
+import java.util.Arrays;
+import java.util.List;
+
 import me.capit.entropy.EntropyMain;
+import me.capit.entropy.Message;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PlayerInput implements CommandExecutor {
 	final EntropyMain plugin;
@@ -14,9 +19,31 @@ public class PlayerInput implements CommandExecutor {
 	
 	public boolean onCommand(CommandSender s, Command cmd, String lbl, String[] args) {
 		if (lbl.equalsIgnoreCase("city")){
-			return true;
+			if (args.length>0 && !args[0].equalsIgnoreCase("?")){
+				if (args[0].equalsIgnoreCase("admin") && s.hasPermission("city.admin")){
+					List<String> tArgs = Arrays.asList(args);
+					tArgs.remove(0);
+					handleAdminCommands(s,cmd,lbl,tArgs.toArray(new String[]{}));
+				} else if(s.hasPermission("city.player")) {
+					handlePlayerCommands(s,cmd,lbl,args);
+				} else {
+					Message.sendError((Player) s, "You do not have permission for this command!");
+				}
+				return true;
+			} else {
+				return onCommand(s,cmd,lbl,new String[]{"help"});
+			}
 		}
 		return false;
 	}
+	
+	private void handlePlayerCommands(CommandSender s, Command cmd, String lbl, String[] args){
+		
+	}
+	
+	private void handleAdminCommands(CommandSender s, Command cmd, String lbl, String[] args){
+		
+	}
+
 
 }
